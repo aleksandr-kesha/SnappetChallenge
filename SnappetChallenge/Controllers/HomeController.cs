d49c4e3ee82b7fc33c66eabf4642e25ef4239d05
Aleksandr Smalyokha <aleksandr.smalyokha@mitratech.com>
2017-03-04 11:58:52 +0700

    Intial commit

---
+++ SnappetChallenge/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+
+namespace SnappetChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DataRepository _dataRepository = new DataRepository();
+
+        public ActionResult Index()
+        {
+            var data = _dataRepository.GetData();
+
+            var nullData = data.Where(d => d.Difficulty.Contains("null") || d.Difficulty.Contains("NULL")).ToList();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SnappetChallenge/Models/DataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnappetChallenge.Models
+{
+    public class DataModel
+    {
+        // subject (аналог учебника) - domain (глава) - sub-domain (раздел) - learning objective (тема) 
+
+        public long SubmittedAnswerId { get; set; }
+        public DateTime SubmitDateTime { get; set; }
+        public bool Correct { get; set; }
+        public int Progress { get; set; }
+        public long UserId { get; set; }
+        public long ExerciseId { get; set; }
+        public string Difficulty { get; set; }
+        public string Subject { get; set; }
+        public string Domain { get; set; }
+        public string LearningObjective { get; set; }
+    }
+}
